b958b3b1452908382d6866ee2f7f04ca8248e523
Danilo Alves <dannalves111@gmail.com>
2022-02-06 10:33:09 -0300

    add: about page and template

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
+import { InfoSquare } from '@styled-icons/boxicons-regular'
+import LinkWrapper from 'components/LinkWrapper'
 import dynamic from 'next/dynamic'
 
 const Map = dynamic(() => import('../components/Map'), { ssr: false })
 
 export default function Home() {
-  return <Map />
+  return (
+    <>
+      <LinkWrapper href="/about">
+        <InfoSquare size={32} aria-label="About" />
+      </LinkWrapper>
+      <Map />
+    </>
+  )
 }
---
+++ src/templates/About/index.tsx
@@ -0,0 +1,22 @@
+import { WindowClose } from '@styled-icons/boxicons-regular'
+import LinkWrapper from 'components/LinkWrapper'
+import * as S from './styles'
+
+const AboutTemplate = () => (
+  <S.Content>
+    <LinkWrapper href="/home">
+      <WindowClose size={32} aria-label="Close" />
+    </LinkWrapper>
+    <S.Heading>Valorant Agents</S.Heading>
+    <S.Body>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis
+        cumque totam, amet maiores reiciendis reprehenderit culpa non labore, ad
+        commodi aperiam aliquam nisi, dolorem excepturi quasi saepe unde
+        temporibus explicabo?
+      </p>
+    </S.Body>
+  </S.Content>
+)
+
+export default AboutTemplate
